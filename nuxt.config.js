1bd4e6544f60578357256dc9822e766ebe403509
Martin Molcrette <mart1.molcrette@gmail.com>
2021-04-25 15:47:43 +0200

    Add keywords

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,14 +1,15 @@
 export default {
     // Global page headers: https://go.nuxtjs.dev/config-head
     head: {
-        title: 'figmadarkui',
+        title: 'Figma Dark UI',
         htmlAttrs: {
             lang: 'en'
         },
         meta: [
             { charset: 'utf-8' },
             { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { hid: 'description', name: 'description', content: '' }
+            { hid: 'description', name: 'description', content: 'Dark UI extension for Figma Webapp. Join the dark side of Figma !' },
+            { hid: 'keywords', name: 'keywords', content: 'figma, dark ui, dark mode, dark, extension, chrome, ui design, ui, ux, ux design' }
         ],
         link: [
             { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
