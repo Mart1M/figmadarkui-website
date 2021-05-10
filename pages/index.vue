df42ab276b3fbbf77e5d0392adb9d1c55ab4b111
Martin Molcrette <mart1.molcrette@gmail.com>
2021-05-10 18:59:30 +0200

    Edit chrome link

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
       <img src="../assets/img/logo.svg" alt="" srcset="" class="mb-16">
     </div>
     <section class="grid grid-cols-1 gap-4 2xl:mx-96 2xl:px-16 lg:mx-32 mx-8 md:grid-cols-3 mb-8 l:mb:0">
-      <a href="https://chrome.google.com/webstore/detail/giimcbodleepjhglbccnkccegcihjall/preview?hl=fr&authuser=0" class="flex items-center rounded-lg bg-gray-900 space-x-4 px-4 py-2">
+      <a href="https://chrome.google.com/webstore/detail/figma-dark-ui/giimcbodleepjhglbccnkccegcihjall?hl=fr" class="flex items-center rounded-lg bg-gray-900 space-x-4 px-4 py-2">
         <img src="../assets/img/chrome.png" alt="" srcset="" class="w-8 h-8">
         <div class="flex flex-col">
           <span class="font-semibold text-white">Download for Chrome</span>
